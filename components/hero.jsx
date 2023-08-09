976862774779bb0eda509356df605d1ec07175e4
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-09 17:24:17 +0600

    added hero section basic

---
+++ components/hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="my-20 flex p-4 gap-10 w-full justify-between items-center">
+      <div className="flex flex-col gap-6 w-[50%] h-full">
+        <p className="text-sky-400">One platform blah blah.</p>
+        <h2 className="text-2xl lg:text-3xl text-slate-900 font-semibold">
+          Improve something Lorem ipsum dolor sit. mone hji kei king my wo
+        </h2>
+        <p className="text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          quos, reprehenderit iusto iste cum consequatur nesciunt alias numquam
+          magnam labore.!!
+        </p>
+      </div>
+      <div className="relative w-[50%] h-full text-center align-middle">BIG POSTER IMAGE</div>
+    </div>
+  );
+};
+
+export default Hero;
