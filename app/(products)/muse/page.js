279c7aea6e7800061a1c65cfe22e1b559868bc5a
jsKraken <javascriptiqbal@gmail.com>
2023-08-10 01:16:20 +0600

    Update UI & Add Custom 404 Page

--- app/(products)/muse/page.js
+++ app/(products)/muse/page.js
@@ -5,12 +5,7 @@ import MuseCard from "./components/card";
 
 const Page = () => {
     return (
-        <div
-            style={{
-                marginTop: "160px",
-            }}
-            className="mb-20 w-full "
-        >
+        <div className="mb-20 w-full custom-top">
             <div className="flex md:flex-row flex-col gap-16 items-center justify-center">
                 <Banner />
             </div>
