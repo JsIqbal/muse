f3ef181e56cd1282128de3d40eaff1044eefc61c
jsKraken <javascriptiqbal@gmail.com>
2023-08-09 18:22:31 +0600

    Resolved Conflicts & Updated Navbar

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
 const Hero = () => {
-  return (
-    <div className="my-20 flex md:flex-row flex-col p-4 gap-16 w-full md:justify-between justify-center items-center">
-      <div className="flex flex-col gap-6 md:w-[45%] w-full h-full">
-        <p className="text-sky-400">One platform blah blah.</p>
-        <h2 className="text-3xl  text-slate-900 font-semibold">
-          Improve something Lorem ipsum dolor sit. mone hji kei king my wo
-        </h2>
-        <p className="text-muted-foreground">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          quos, reprehenderit iusto iste cum consequatur nesciunt alias numquam
-          magnam labore.!!
-        </p>
-      </div>
-      <div className="relative md:w-[55%] w-full h-full text-center align-middle">BIG POSTER IMAGE</div>
-    </div>
-  );
+    return (
+        <div className="my-20 flex md:flex-row flex-col p-4 gap-16 w-full md:justify-between justify-center items-center">
+            <div className="flex flex-col gap-6 md:w-[45%] w-full h-full">
+                <p className="text-sky-400">One platform blah blah.</p>
+                <h2 className="text-3xl  text-slate-900 font-semibold">
+                    Improve something Lorem ipsum dolor sit. mone hji kei king
+                    my wo
+                </h2>
+                <p className="text-muted-foreground">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae quos, reprehenderit iusto iste cum consequatur
+                    nesciunt alias numquam magnam labore.!!
+                </p>
+            </div>
+            <div className="relative md:w-[55%] w-full h-full text-center align-middle">
+                BIG POSTER IMAGE
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,70 +6,70 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 import { Montserrat } from "next/font/google";
 import { useState } from "react";
 
 const montsserrat = Montserrat({
-  weight: "700",
-  subsets: ["latin"],
+    weight: "700",
+    subsets: ["latin"],
 });
 
 const Navbar = () => {
-  const [position, setPosition] = useState("top");
+    const [position, setPosition] = useState("top");
 
-  return (
-    <div className="flex justify-between  p-4 bg-transparent w-full">
-      <Link 
-        href="/"
-        className="flex items-center justify-center gap-4 mb-16 mt-6"
-      >
-        <div className="relative w-16 h-8 ">
-          <Image fill alt="Logo" src="/icon.webp" />
-        </div>
-        <h1 className={cn("text-2xl font-bold", montsserrat.className)}>
-          MUSE
-        </h1>
-      </Link>
-      <div className="flex gap-2 p-4">
-        <DropdownMenu className="">
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline">Open</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>Our Products</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuRadioGroup
-              value={position}
-              onValueChange={setPosition}
+    return (
+        <div className="flex justify-between  p-4 bg-transparent w-full">
+            <Link
+                href="/"
+                className="flex items-center justify-center gap-4 mb-16 mt-6"
             >
-              <DropdownMenuRadioItem value="top">
-                <Link href="/product/1">Product 1</Link>
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="bottom">
-                <Link href="/product/2">Product 2</Link>
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="right">
-                <Link href="/product/3">Product 3</Link>
-              </DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
+                <div className="relative w-16 h-8 ">
+                    <Image fill alt="Logo" src="/icon.webp" />
+                </div>
+                <h1 className={cn("text-2xl font-bold", montsserrat.className)}>
+                    MUSE
+                </h1>
+            </Link>
+            <div className="flex gap-2 p-4">
+                <DropdownMenu className="">
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline">Open</Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>Our Products</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup
+                            value={position}
+                            onValueChange={setPosition}
+                        >
+                            <DropdownMenuRadioItem value="top">
+                                <Link href="/muse">muse</Link>
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="bottom">
+                                <Link href="/jet-pack">jet-pack</Link>
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="right">
+                                <Link href="/blackbeltprivecy">
+                                    blackbeltprivecy
+                                </Link>
+                            </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
 
-        <Button className="bg-blue-700 ml-8">
-            Contact us
-        </Button>
-      </div>
-    </div>
-  );
+                <Button className="bg-blue-700 ml-8">Contact us</Button>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
