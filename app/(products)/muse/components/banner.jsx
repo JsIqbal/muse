2f5a40dc3f3258ae30244e0c1b146c54414d7b2d
jsKraken <javascriptiqbal@gmail.com>
2023-08-09 23:11:53 +0600

    Update Layout & UI

--- app/(products)/muse/components/banner.jsx
+++ app/(products)/muse/components/banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     return (
         <>
             <div className="flex flex-col gap-6 md:w-[45%] w-full h-full">
-                <div class="text-left text-blue-600 font-semibold text-lg">
+                <div className="text-left text-blue-600 font-semibold text-lg">
                     <Badge
                         className="border-2 border-emerald-500 rounded-full  text-emerald-500 text-lg font-bold"
                         variant="outline"
@@ -18,24 +18,24 @@ const Banner = () => {
                         use
                     </span>
                 </div>
-                <div class="mt-4">
-                    <h1 class="text-3xl font-bold">
+                <div className="mt-4">
+                    <h1 className="text-3xl font-bold">
                         Muse: Middleware Universal Scripting idE
                     </h1>
                 </div>
-                <div class="mt-2">
-                    <h3 class="text-lg text-gray-700">
+                <div className="mt-2">
+                    <h3 className="text-lg text-gray-700">
                         DevOps Automate: WebSphere; WebLogic; JBoss; Glassfish;
                         Tomcat; Linux.
                     </h3>
                 </div>
-                <div class="mt-4">
-                    <Button class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
+                <div className="mt-4">
+                    <Button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
                         Download
                     </Button>
                 </div>
-                <div class="mt-4">
-                    <p class="text-gray-600">Brought to you by: musesoft</p>
+                <div className="mt-4">
+                    <p className="text-gray-600">Brought to you by: musesoft</p>
                 </div>
             </div>
             <div className="relative md:w-[55%] w-full h-full text-center align-middle ">
--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="text-center bg-gray-800 text-white py-2  bottom-0 w-full">
+        <div className="fixed bottom-0 left-0 right-0 text-center bg-gray-800 text-white py-2 w-full">
             &copy; 2023 all rights reserved by CAV
         </div>
     );
