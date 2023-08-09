cfdc3234ce6bc57546eb07585b29a38e22fef18d
jsKraken <javascriptiqbal@gmail.com>
2023-08-09 18:51:34 +0600

    Update the Root Layout & Add Footer

---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="text-center bg-gray-800 text-white py-2  bottom-0 w-full">
+            &copy; 2023 all rights reserved by CAV
+        </div>
+    );
+};
+
+export default Footer;
