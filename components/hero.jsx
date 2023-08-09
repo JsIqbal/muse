c157e6008a9425ae5a0615a6139abf0e901dc54d
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-09 17:38:40 +0600

    Upgraded the hero section

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="my-20 flex p-4 gap-10 w-full justify-between items-center">
-      <div className="flex flex-col gap-6 w-[50%] h-full">
+    <div className="my-20 flex md:flex-row flex-col p-4 gap-16 w-full md:justify-between justify-center items-center">
+      <div className="flex flex-col gap-6 md:w-[45%] w-full h-full">
         <p className="text-sky-400">One platform blah blah.</p>
-        <h2 className="text-2xl lg:text-3xl text-slate-900 font-semibold">
+        <h2 className="text-3xl  text-slate-900 font-semibold">
           Improve something Lorem ipsum dolor sit. mone hji kei king my wo
         </h2>
         <p className="text-muted-foreground">
@@ -14,7 +14,7 @@ const Hero = () => {
           magnam labore.!!
         </p>
       </div>
-      <div className="relative w-[50%] h-full text-center align-middle">BIG POSTER IMAGE</div>
+      <div className="relative md:w-[55%] w-full h-full text-center align-middle">BIG POSTER IMAGE</div>
     </div>
   );
 };
