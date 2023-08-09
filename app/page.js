6849de1ae52a9a771d44d33fc770af73c1200e53
jsKraken <javascriptiqbal@gmail.com>
2023-08-09 17:22:52 +0600

    Resolve conflicts & Update Nav

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>hello</div>
-  )
+    return <div>hello</div>;
 }
---
+++ app/products/muse/layout.js
@@ -0,0 +1,7 @@
+"use client";
+
+const Layout = ({ children }) => {
+    return <div>{children}</div>;
+};
+
+export default Layout;
