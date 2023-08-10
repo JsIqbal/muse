d5acf2c4e043b04d8615ca853bfa843146d3494d
jsKraken <javascriptiqbal@gmail.com>
2023-08-10 12:32:34 +0600

    Add Tabs in Muse

---
+++ app/(products)/muse/components/tab.jsx
@@ -0,0 +1,11 @@
+import CommonTabs from "@/components/tabs";
+
+const MuseTab = () => {
+    return (
+        <div className="w-full mt-16 bg-gray-100 bg-opacity-93">
+            <CommonTabs />
+        </div>
+    );
+};
+
+export default MuseTab;
