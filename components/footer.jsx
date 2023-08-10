a780cc151aae5cd9be95d6a01d7f7532f2d34f7b
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-10 16:15:07 +0600

    Added shadow and border in navbar, and footer class changed

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="fixed bottom-0 left-0 right-0 text-center bg-gray-800 text-white py-2 w-full">
+        <div className="mt-auto text-center bg-gray-800 text-white py-2 w-full">
             &copy; 2023 all rights reserved by CAV
         </div>
     );
