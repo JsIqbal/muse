73d9b2552214c73a8606e5870b8de9ad1bb6f3eb
jsKraken <javascriptiqbal@gmail.com>
2023-08-10 12:32:34 +0600

    Add Tabs in Muse

---
+++ components/tabs.jsx
@@ -0,0 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const CommonTabs = () => {
+    return (
+        <Tabs defaultValue="account" className="w-[400px]">
+            <TabsList>
+                <TabsTrigger value="account">Account</TabsTrigger>
+                <TabsTrigger value="password">Password</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">
+                Make changes to your account here.
+            </TabsContent>
+            <TabsContent value="password">
+                Change your password here.
+            </TabsContent>
+        </Tabs>
+    );
+};
+
+export default CommonTabs;
