3a0e011a993584491057bc67b0ec4fd79a65b5ee
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-10 19:09:34 +0600

    added review section

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 hover:scale-[102%] shadow-lg transition duration-150",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground hover:scale-[102%] shadow-lg transition-all duration-150",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
