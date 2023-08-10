ac566d32574d30e6d401878475661004199747f9
jsKraken <javascriptiqbal@gmail.com>
2023-08-10 12:42:13 +0600

    Make Dynamic Tab System

--- app/(products)/muse/components/tab.jsx
+++ app/(products)/muse/components/tab.jsx
@@ -1,9 +1,20 @@
 import CommonTabs from "@/components/tabs";
 
 const MuseTab = () => {
+    const tabsData = {
+        tabList: [
+            { value: "account", label: "Account" },
+            { value: "password", label: "Password" },
+        ],
+        tabContent: [
+            { value: "account", text: "Make changes to your account here." },
+            { value: "password", text: "Change your password here." },
+        ],
+    };
+
     return (
         <div className="w-full mt-16 bg-gray-100 bg-opacity-93">
-            <CommonTabs />
+            <CommonTabs tabsData={tabsData} />
         </div>
     );
 };
