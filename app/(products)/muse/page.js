f93db17c41cc8bd4b39c4372f58ca99cc3d7b15c
jsKraken <javascriptiqbal@gmail.com>
2023-08-10 12:42:13 +0600

    Make Dynamic Tab System

--- app/(products)/muse/page.js
+++ app/(products)/muse/page.js
@@ -6,7 +6,7 @@ import MuseTab from "./components/tab";
 
 const Page = () => {
     return (
-        <div className="mb-20 w-full custom-top">
+        <div className="mb-20 w-full custom-top ">
             <div className="flex md:flex-row flex-col gap-16 items-center justify-center">
                 <Banner />
             </div>
