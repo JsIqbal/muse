74b6c1aacf2eba219869f0c5df822d87296b0cbd
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-12 01:08:44 +0600

    Resolving conflict -5

--- app/(products)/layout.js
+++
@@ -1,5 +0,0 @@
-const Layout = ({ children }) => {
-    return <div className="flex-grow container">{children}</div>;
-};
-
-export default Layout;
--- components/footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <div className="mt-auto text-center bg-gray-800 text-white py-2 w-full">
-            &copy; 2023 all rights reserved by CAV
-        </div>
-    );
-};
-
-export default Footer;
