e7c3710b578918c1602d43f576ef0fe9cfa9b3fc
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-12 00:40:50 +0600

    Upgraded card component

--- client/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.jsx
+++ client/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.jsx
@@ -3,9 +3,9 @@
 import { SignUp, useAuth } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
-export default function Page() {
+export default function Page({ params }) {
 
-  const { isSignedIn } = useAuth({ params });
+  const { isSignedIn } = useAuth();
   const router = useRouter();
 
   if (isSignedIn) {
