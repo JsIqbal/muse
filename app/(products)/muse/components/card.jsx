8f837ca6a8f1176936db11e16a5f4dfea837f649
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-11 15:18:20 +0600

    New Setup

--- app/(products)/muse/components/card.jsx
+++
@@ -1,9 +0,0 @@
-import CommonCard from "@/components/card";
-
-const MuseCard = () => {
-    return (
-        <CommonCard className="w-full mt-16 bg-gray-100 bg-opacity-93 text-dark" />
-    );
-};
-
-export default MuseCard;
