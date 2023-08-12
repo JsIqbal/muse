059b808a02f95ac8b53ba03b5f36d2e061ecef5f
jsKraken <javascriptiqbal@gmail.com>
2023-08-13 04:17:52 +0600

    Resolve Conflicts

--- server/rest/user/controller.go
+++ server/rest/user/controller.go
@@ -10,26 +10,26 @@ import (
 	"encoding/json"
 )
 
-func getDashboardImages(service svc.Service) gin.HandlerFunc {
+func createUser(service svc.Service) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var user svc.User
 
-	body, err := ctx.GetRawData()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read request body"})
-		return
-	}
+		body, err := ctx.GetRawData()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read request body"})
+			return
+		}
 
-	err = json.Unmarshal(body, &user)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
-		return
-	}
+		err = json.Unmarshal(body, &user)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+			return
+		}
 
-	fmt.Printf("Username: %s\nPassword: %s\n", user.Username, user.Password)
-	
-	service.CreateUser(&user)
+		fmt.Printf("Username: %s", user.Email)
+		
+		service.CreateUser(&user)
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
+		ctx.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
 	}
 }
