24af848e7099ecd1bf92512123d6f017088c6142
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-17 23:46:54 +0600

    CLIENT: Added blinking states to cart

--- client/components/cart.jsx
+++ client/components/cart.jsx
@@ -3,8 +3,10 @@
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { ShoppingBagIcon } from "@/node_modules/lucide-react";
+import useCart from "@/hooks/use-cart";
 
 const Cart = () => {
+    const cart = useCart();
     const router = useRouter();
     return (
         <Button
@@ -13,8 +15,17 @@ const Cart = () => {
             className="space-x-2 relative"
         >
             <ShoppingBagIcon />
-            <span className=" h-max w-max font-semibold text-slate-900">
+            <span className=" h-max w-max font-semibold text-slate-900" onClick={() => {cart.setCartBlinking(false) }}>
                 Go to cart{" "}
+                {cart.blink && (
+                    <div className="absolute top-0 right-0">
+                        <span
+                            className="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-blue-400 opacity-75"
+                            style={{ animationDuration: "2s" }}
+                        ></span>
+                        <span className="absolute inline-flex rounded-full h-3 w-3 bg-blue-500"></span>
+                    </div>
+                )}
             </span>
         </Button>
     );
