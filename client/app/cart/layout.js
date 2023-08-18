9496c70b04ed29c11d1fb82604a518c4cd78615b
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-18 23:40:55 +0600

    CLIENT: Fixed Hidration Error And added loading spinner

---
+++ client/app/cart/layout.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const CartLayout = ({ children }) => {
+    const [isHydrated, setIsHydrated] = useState(false);
+
+    //Wait till NextJS rehydration completes
+    useEffect(() => {
+        setIsHydrated(true);
+    }, []);
+    return (
+        <>
+            {isHydrated ? (
+                <div className="w-full h-full">{children}</div>
+            ) : (
+                <div className="w-full h-full flex justify-center items-center">
+                    <span
+                        style={{ animationDuration: "3s" }}
+                        className="w-16 h-16 border-[6px] border-dashed border-blue-700 rounded-full animate-spin"
+                    ></span>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default CartLayout;
