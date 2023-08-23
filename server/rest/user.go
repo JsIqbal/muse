c33bdb83a7be096105dddf449f88091009a6018f
jsKraken <javascriptiqbal@gmail.com>
2023-08-23 16:48:49 +0600

    SERVER: Add Authorization:

--- server/rest/user.go
+++ server/rest/user.go
@@ -117,3 +117,15 @@ func (s *Server) purchase(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "Purchases successfully created"})
 }
 
+// logout godoc
+// @Summary Log out the admin
+// @Description Log out the user by removing the token cookie from the browser
+// @Tags admin
+// @Accept json
+// @Produce json
+// @Success 200 {object} SuccessResponse
+// @Router /api/users/logout [post]
+func (s *Server) logout(ctx *gin.Context) {
+	ctx.SetCookie("token", "", -1, "/", "", false, true)
+	ctx.JSON(http.StatusOK, gin.H{"message": "Logged out successfully"})
+}
